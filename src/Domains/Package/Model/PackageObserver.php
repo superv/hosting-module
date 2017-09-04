e7cce407409428c3a16e3b6b764c107f904eae8c
Ali Selcuk <maselcuk@gmail.com>
2017-09-05 00:34:47 +0300

    try higher order proxy

--- src/Domains/Package/Model/PackageObserver.php
+++ src/Domains/Package/Model/PackageObserver.php
@@ -2,8 +2,6 @@
 
 namespace SuperV\Modules\Hosting\Domains\Package\Model;
 
-use PhpParser\Builder\Function_;
-use SuperV\Modules\Supreme\Domains\Drop\Model\DropModel;
 use SuperV\Platform\Domains\Entry\EntryModel;
 use SuperV\Platform\Domains\Entry\EntryObserver;
 
@@ -12,9 +10,11 @@ class PackageObserver extends EntryObserver
     public function deleted(EntryModel $entry)
     {
         /** @var PackageModel $entry */
-        $entry->getParts()->map(function (PartModel $part) {
-            $part->delete();
-        });
+        $entry->getParts()->map->delete();
+
+//        $entry->getParts()->map(function (PartModel $part) {
+//            $part->delete();
+//        });
         //$entry->getDrops()->map(function (DropModel $drop) use ($entry) {
         //    $entry->drops()->detach($drop);
         //    $drop->delete();
